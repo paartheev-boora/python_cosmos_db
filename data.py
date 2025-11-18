11e4230efd8a06df82b87fd39e7f698a3d13079c
paartheev-boora <partheevboora@gmail.com>
2025-11-18 14:33:51 +0530

    null_values

---
+++ data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+og_data = os.getenv("FULL_PATH")
+sample_data = os.getenv("NULL_DATA")
+
+np.random.seed(42)
+n = 100
+
+data = {
+    "engine_cc": np.random.randint(800, 3500, n),
+    "vehicle_weight_kg": np.random.randint(700, 2500, n),
+    "cylinders": np.random.choice([3, 4, 6], n, p=[0.2, 0.6, 0.2]),
+    "fuel_type": np.random.choice(["Petrol", "Diesel", "CNG"], n, p=[0.5, 0.4, 0.1]),
+    "tire_pressure_psi": np.random.randint(28, 38, n),
+}
+
+# Base mileage formula with noise
+mileage = (
+    50
+    - (data["engine_cc"] / 200) 
+    - (data["vehicle_weight_kg"] / 300)
+    - (data["cylinders"] * 0.8)
+    + ((np.array(data["tire_pressure_psi"]) - 32) * 0.3)
+    + np.random.normal(0, 1.5, n)
+)
+
+data["mileage_kmpl"] = np.round(mileage, 2)
+
+df = pd.DataFrame(data)
+df.to_csv(og_data, index=False)
+print(df)
+
+# Make 20% of mileage null
+np.random.seed(42)
+null_indices = np.random.choice(df.index, size=int(0.2 * len(df)), replace=False)
+df.loc[null_indices, "mileage_kmpl"] = np.nan
+
+# Save new file
+df.to_csv(sample_data, index=False)
+print(df[df["mileage_kmpl"].isna()].head(10))
---
+++ main.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from bson import ObjectId
+import pandas as pd
+import time
+load_dotenv()
+
+CONN_STRING = os.getenv("COSMOS_CONN_STRING")
+DB_NAME = os.getenv("DB_NAME")
+COLL_NAME = os.getenv("COLLECTION")
+
+app = FastAPI()
+client = AsyncIOMotorClient(CONN_STRING, tls=True, tlsAllowInvalidCertificates=True)
+db = client[DB_NAME]
+collection = db[COLL_NAME]
+
+def fix_id(doc):
+    doc["id"] = str(doc["_id"])
+    del doc["_id"]
+    return doc
+
+@app.get("/")
+async def root():
+    return {"message": "Cosmos App running"}
+
+# Insert
+@app.post("/cars")
+async def create_product(product: dict):
+    cars=pd.read_csv("car_mileage_with_nulls.csv")
+    cars_dicts=cars.to_dict(orient="records")
+    for i in range(0, len(cars_dicts), 30):
+        batch = cars_dicts[i:i + 30]
+        result=collection.insert_many(batch)
+        time.sleep(0.2)
+    return {"id:": str(len(result.inserted_id))}
+
+# Read
+@app.get("/cars")
+async def read_product():
+    cursor = await collection.find().to_list(100)
+    return [fix_id(p) for p in cursor]
+
+# # Read a product
+# @app.get("/products/{id}")
+# async def get_product(id: str):
+#     product = await collection.find_one({"_id": ObjectId(id)})
+#     if not product:
+#         raise HTTPException(404, "Product not Found")
+#     return fix_id(product)
+
+# # Update
+# @app.put("/products/{id}")
+# async def update_item(id: str, data: dict):
+#     result = await collection.update_one(
+#         {"_id": ObjectId(id)},
+#         {"$set": data}
+#     )
+#     if result.modified_count == 0:
+#         raise HTTPException(404, "Product not Found")
+#     return {"messsage": "Updated"}
+
+# # Delete
+# @app.delete("/products/{id}")
+# async def delete_product(id: str):
+#     result = await collection.delete_one({"_id": ObjectId(id)})
+#     if result.deleted_count == 0:
+#         raise HTTPException(404, "Product not Found")
+#     return {"message": "Deleted Successfully"}
+
